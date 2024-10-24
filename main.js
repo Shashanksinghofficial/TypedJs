79e54cc2afbf7800a9156c2a449f2163d82724ff
Ankit Kosta <codetive3@gmail.com>
2024-10-24 12:10:07 +0530

    Upload Typed js code

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".element", {
+      strings: ["Ankit Kosta.", "A Coder.", "A Software Engineer", "A Hacker", "Anonymous"],
+      typeSpeed: 50,
+      backSpeed: 25,
+      loop: true
+    });
